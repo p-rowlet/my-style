0aa33fd5739d904f8d95478a56054f35a2ad69e9
rowlet <kagechi@naver.com>
2022-03-08 20:28:23 +0900

    헤더 및 푸터 스타일링

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,8 +1,12 @@
-import React from 'react';
-import styles from './footer.module.css';
+import React from "react";
+import styles from "./footer.module.css";
 
 const Footer = (props) => {
-    return <footer className={styles.footer}>푸터</footer>
+	return (
+		<footer className={styles.footer}>
+			<p className={styles.copyright}>copyright© rowlet-p ALL RIGHTS RESERVED</p>
+		</footer>
+	);
 };
 
 export default Footer;
--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,8 +1,16 @@
-import React from 'react';
-import styles from './header.module.css';
+import React from "react";
+import styles from "./header.module.css";
+import logo from "./../../logo.svg";
 
-const Header = (props) =>{
-    return <header className={styles.header}>mystyle</header>
+const Header = (props) => {
+	return (
+		<header className={styles.header}>
+			<div className={styles.content}>
+				<img className={styles.logoimg} src={logo} alt="MyStyle 로고" />
+				<button className={styles.logout}>Logout</button>
+			</div>
+		</header>
+	);
 };
 
 export default Header;
