664182d7b7670f59c0e159368a4c372fed5055a3
rowlet <kagechi@naver.com>
2022-03-08 23:33:52 +0900

    로그인창 UI 구성(임시)

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -1,7 +1,19 @@
-import React from 'react';
+import React from "react";
+import styles from "./login.module.css";
 
 const Login = (props) => {
-    return <div>로그인</div>
-}
+	return (
+		<section>
+			<div className={styles.introduce}>
+				<h1>나만의 스타일 시안을 구상해보세요!</h1>
+				<div className={styles.login}>
+					<button><img src="images/google_logo.png" alt="" /> Google 계정으로 로그인</button>
+					<button><img src="images/twitter_logo.png" alt="" /> 트위터 계정으로 로그인</button>
+					<button><img src="images/facebook_logo.png" alt="facebook_logo" /> 페이스북 계정으로 로그인</button>
+				</div>
+			</div>
+		</section>
+	);
+};
 
 export default Login;
