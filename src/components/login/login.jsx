da4dc979dae62bbec1ad7b4629adcf5e5446a927
rowlet <kagechi@naver.com>
2022-03-11 01:44:48 +0900

    로그인 창 내용 수정 트위터>깃허브

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -8,8 +8,8 @@ const Login = (props) => {
 				<h1>나만의 스타일 시안을 구상해보세요!</h1>
 				<div className={styles.login}>
 					<button><img src="images/google_logo.png" alt="google_logo" /> Google 계정으로 로그인</button>
-					<button><img src="images/twitter_logo.png" alt="twitter_logo" /> 트위터 계정으로 로그인</button>
-					<button><img src="images/facebook_logo.png" alt="facebook_logo" /> 페이스북 계정으로 로그인</button>
+					<button><img src="images/github_logo.png" alt="github_logo" /> Github 계정으로 로그인</button>
+					<button><img src="images/facebook_logo.png" alt="facebook_logo" /> Facebook 계정으로 로그인</button>
 				</div>
 			</div>
 		</section>
