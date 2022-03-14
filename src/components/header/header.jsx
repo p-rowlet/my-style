513d6607c383d272fdc1619f0441129675e6fb63
rowlet <kagechi@naver.com>
2022-03-14 19:51:02 +0900

    헤더 기능성 수정(임시)

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -2,12 +2,23 @@ import React from "react";
 import styles from "./header.module.css";
 import logo from "./../../logo.svg";
 
-const Header = ({onLogout}) => {
+const Header = ({ onLogout }) => {
 	return (
 		<header className={styles.header}>
 			<div className={styles.content}>
-				<img className={styles.logoimg} src={logo} alt="MyStyle 로고" />
-				{onLogout && <button className={styles.logout} onClick={onLogout}>Logout</button>}
+				<a href="/">
+					<img
+						className={styles.logoimg}
+						src={logo}
+						alt="MyStyle 로고"
+					/>
+				</a>
+
+				{onLogout && (
+					<button className={styles.logout} onClick={onLogout}>
+						Logout
+					</button>
+				)}
 			</div>
 		</header>
 	);
